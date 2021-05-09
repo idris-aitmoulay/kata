2924fb4056c9e991895d621d5aee748e520c7c6c
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-05-09 01:07:08 +0000

    add success test and check idemtency function

--- fizzbuzz/js/src/__tests__/fizzbuzz.test.js
+++ fizzbuzz/js/src/__tests__/fizzbuzz.test.js
@@ -18,4 +18,34 @@ describe('fizzbuzz function', () => {
       fizzbuzz(12.5)
     }).toThrow("12.5 is non acceptable by fizzbuzz function as param")
   })
+
+  it('test: given Fizz, Buzz or FizzBuzz it should return the same value', () => {
+    expect(fizzbuzz('Fizz')).toStrictEqual('Fizz')
+    expect(fizzbuzz('Buzz')).toStrictEqual('Buzz')
+    expect(fizzbuzz('FizzBuzz')).toStrictEqual('FizzBuzz')
+  })
+
+
+  it('given non divisible int by 3, 5 or 15 it should return string of given number', () => {
+    expect(fizzbuzz(13)).toStrictEqual('13')
+  })
+
+  it('given divisible int by only 3 not 5 it should return Fizz', () => {
+    expect(fizzbuzz(12)).toStrictEqual('Fizz')
+  })
+
+  it('given divisible int by only 5 not 3 it should return Buzz', () => {
+    expect(fizzbuzz(5)).toStrictEqual('Buzz')
+  })
+
+  it('given divisible int by  15 it should return FizzBuzz', () => {
+    expect(fizzbuzz(30)).toStrictEqual('FizzBuzz')
+  })
+
+  it('test fizzbuzz is an Idempotency function', () => {
+    expect(fizzbuzz(fizzbuzz(30))).toStrictEqual(fizzbuzz(30))
+    expect(fizzbuzz(fizzbuzz(5))).toStrictEqual(fizzbuzz(5))
+    expect(fizzbuzz(fizzbuzz(12))).toStrictEqual(fizzbuzz(12))
+    expect(fizzbuzz(fizzbuzz(13))).toStrictEqual(fizzbuzz(13))
+  })
 })
--- fizzbuzz/js/src/fizzbuzz.js
+++ fizzbuzz/js/src/fizzbuzz.js
@@ -6,7 +6,19 @@ const fizzbuzz = value  => {
     throw new Error(`${value} is non acceptable by fizzbuzz function as param`);
   }
 
-  return '0'
+  if (isInFizzBuzzValues) return `${value}`;
+
+  const usedValue = (typeof value === 'string') ? parseInt(value): value;
+
+  const divisibleBy3 = usedValue % 3 === 0, divisibleBy5 = usedValue % 5 === 0;
+
+  if (divisibleBy3 && divisibleBy5) return 'FizzBuzz';
+
+  if (divisibleBy3) return 'Fizz';
+
+  if (divisibleBy5) return 'Buzz';
+
+  return `${usedValue}`;
 };
 
 module.exports = fizzbuzz;
