6d8d542ac976d2acf5772fac32f534eb588306bb
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-05-09 01:08:38 +0000

    update main function.

--- fizzbuzz/js/src/app.js
+++ fizzbuzz/js/src/app.js
@@ -2,9 +2,10 @@ const fizzbuzz = require('./fizzbuzz');
 
 // Implement the wrapper so we can count from 0 to STOP.
 function main() {
-   for(let i=0; i<10;i++) {
-     console.log(fizzbuzz(i));
-   }
+  const testDataSet = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz"]
+  testDataSet.forEach((item, index) => {
+    console.log('(given: ', item, ', item result: ',fizzbuzz(item), ',index + 1: ', index+1, ',index + 1 result: ',fizzbuzz(index+1))
+  });
 }
 
 main();
